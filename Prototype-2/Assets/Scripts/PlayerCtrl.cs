408e1d44d43d5d1db88572e16d14019b6fabb68d
MadQuokka <snikers25@naver.com>
2019-09-10 17:47:05 +0900

    Instantiate update
    Shooting bullet & Object Instantiate added.

---
+++ Prototype-2/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    public float speed = 20.0f;
+    private float hInput;
+    //private float vInput;
+    public float xRange = 18.0f;
+    public GameObject projectilePrefab;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+
+        //vInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.forward * vInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
